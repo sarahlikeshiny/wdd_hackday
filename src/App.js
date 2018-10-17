ca559d7764e80bde6ebc786ec8426cb3af86ecbc
sarah-miller <sarah.miller@ovoenergy.com>
2018-10-17 16:02:23 +0100

    undo silly merge fail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Slider from '@material-ui/lab/Slider'
 
-require('create-react-class');
+const styles = {
+  root: {
+    width: 300,
+  },
+  slider: {
+    padding: '22px 0px',
+  },
+};
 class App extends Component {
   state = {
-    value: 10,
+    value: 50,
   };
   handleChange = (event, value) => {
     this.setState({ value });
@@ -14,25 +22,26 @@ class App extends Component {
   render() {
     const { classes } = this.props;
     const { value } = this.state;
+
     console.log(value);
 
     return (
       <div className="App">
         <div>
-          <Slider
-            classes={{ container: classes.slider }}
-            value={value}
-            aria-labelledby="label"
-            onChange={this.handleChange}
-            min={0}
-            max={6}
-            step={1}
-          />
-        </div>
-        <div>
           <Button variant="contained" color="primary">
             Hello World
          </Button>
+          <div>
+            <Slider
+              classes={{ container: classes.slider }}
+              value={value}
+              aria-labelledby="label"
+              onChange={this.handleChange}
+              min={0}
+              max={6}
+              step={1}
+            />
+          </div>
         </div>
       </div>
     );
